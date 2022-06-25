70a8c1d04861be42639eadc29c6af0a2f43ab29f
Radio <103074097+Radi0o@users.noreply.github.com>
2022-06-25 12:34:41 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ module.exports = {
     botPrefix: "!",
     ownerID: "YOUR_ID",
     embedColor: "#00dbff",
-    embedfooterText: "Discord.js v13 Example by Expectatives#1157"
+    embedfooterText: "Discord.js v13 Example by Radi0o#9442"
 }
